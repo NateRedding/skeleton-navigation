263ac1a88dc68fd0bbe3e1df216580ddf428fe26
Nathan Redding <nateredding@gmail.com>
2016-01-05 10:15:59 -0500

    A simple test case showing that Aurelia can sometimes wind up changing null bound variables to empty strings if the model object is replaced as a whole.

--- skeleton-es2016/src/welcome.js
+++ skeleton-es2016/src/welcome.js
@@ -1,34 +1,33 @@
-//import {computedFrom} from 'aurelia-framework';
 
 export class Welcome {
   heading = 'Welcome to the Aurelia Navigation App!';
-  firstName = 'John';
-  lastName = 'Doe';
-  previousValue = this.fullName;
 
-  //Getters can't be directly observed, so they must be dirty checked.
-  //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-  //To optimize by declaring the properties that this getter is computed from, uncomment the line below
-  //as well as the corresponding import above.
-  //@computedFrom('firstName', 'lastName')
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
+  activate() {
+    this.name = {
+      first: null,
+      last: null
+    };
+    this.pristine = {
+      first: null,
+      last: null
+    };
   }
 
-  submit() {
-    this.previousValue = this.fullName;
-    alert(`Welcome, ${this.fullName}!`);
+  get isPristine() {
+    return this.areEqual(this.name, this.pristine);
   }
 
-  canDeactivate() {
-    if (this.fullName !== this.previousValue) {
-      return confirm('Are you sure you want to leave?');
-    }
+  // borrowed from app-contacts
+  areEqual(obj1, obj2) {
+    return Object.keys(obj1).every((key) => obj2.hasOwnProperty(key) && (obj1[key] === obj2[key]))
   }
-}
 
-export class UpperValueConverter {
-  toView(value) {
-    return value && value.toUpperCase();
+  meltdown() {
+    this.name = {
+      first: null,
+      last: null
+    };
+    // when this.name is set to a new object, any input fields that are bound to a null value are subsequently given the
+    // value '' and therefore our pristine copy no longer is equal to our pristine copy.
   }
 }
